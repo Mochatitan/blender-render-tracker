73b7ac2f350170c1c12f6f244705f57e155d0d58
Mochatitan <mochatitanstudios@gmail.com>
2025-09-09 23:59:35 -0400

    Work done on logo
    Changed logo and played around with expo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,8 +12,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/squaredefaultcube.png')}
+          style={styles.trackerLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
@@ -38,7 +38,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
+          {`Tap the Explore tab to learn more about what's included in this  bad app.`}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -62,14 +62,14 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   stepContainer: {
-    gap: 8,
+    gap: 2,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
+  trackerLogo: {
+    height: 100,
+    width: 100,
     bottom: 0,
-    left: 0,
+    left: 15,
     position: 'absolute',
   },
 });
